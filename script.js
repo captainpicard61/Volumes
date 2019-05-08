2225e1711e6b141032973bd77d30f4bff20f0c75
Samuel Anderson <samuela820@gmail.com>
2019-05-08 21:47:55 +0000

    Sam git gud at JS

---
+++ script.js
@@ -0,0 +1,12 @@
+$.ajax({
+    url: "<https://www.googleapis.com/books/v1/volumes?q=harry%20potter>",
+    method:"GET",
+    success: function() {
+        book.forEach.
+        books.items[0].volumeInfo.title
+    },
+        
+    
+    
+}
+
